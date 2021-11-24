8da2bcbf69f89c36ff50d4ce1634f3efd596db6e
Harsh K <hkmkeswani@myseneca.ca>
2021-11-24 17:54:53 +0530

    updated template

--- advent2020/template.ts
+++ advent2020/template.ts
@@ -9,8 +9,28 @@ async function processLineByLine() {
     crlfDelay: Infinity,
   });
 
+  let data: string[] = [];
   for await (const line of rl) {
-    
+    data.push(line);
   }
+
 }
+
+async function processLineByLine2() {
+  const fileStream = fs.createReadStream("inputs/day-x.txt");
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  let data: string[] = [];
+  for await (const line of rl) {
+    data.push(line);
+  }
+
+  
+}
+
 processLineByLine();
+// processLineByLine2();
