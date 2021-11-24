9bbe4179ed4242ed5a77df463f10a98b8b389c00
Harsh K <hkmkeswani@myseneca.ca>
2021-11-24 17:54:08 +0530

    day3: completed p1

--- advent2020/day-3.ts
+++ advent2020/day-3.ts
@@ -2,15 +2,50 @@ import fs = require("fs");
 import readline = require("readline");
 
 async function processLineByLine() {
-  const fileStream = fs.createReadStream("inputs/day-x.txt");
+  const fileStream = fs.createReadStream("inputs/day-3.txt");
 
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
   });
 
+  let data: string[] = [];
   for await (const line of rl) {
-    
+    data.push(line);
   }
+
+  let count: number = 0;
+  for (let i: number = 0; i < data.length - 1; i++) {
+    // console.log(data[i + 1][(3 * (i + 1)) % data[0].length] + " ");
+    if (data[i + 1][(3 * (i + 1)) % data[0].length] === "#") {
+      count++;
+    }
+  }
+  console.log(count);
 }
+
+async function processLineByLine2() {
+  const fileStream = fs.createReadStream("inputs/day-3.txt");
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  let data: string[] = [];
+  for await (const line of rl) {
+    data.push(line);
+  }
+
+  let count: number = 0;
+  for (let i: number = 0; i < data.length - 1; i++) {
+    // console.log(data[i + 1][(3 * (i + 1)) % data[0].length] + " ");
+    if (data[i + 1][(3 * (i + 1)) % data[0].length] === "#") {
+      count++;
+    }
+  }
+  console.log(count);
+}
+
 processLineByLine();
+processLineByLine2();
