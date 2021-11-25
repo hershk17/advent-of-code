8155fa7d00ce017d57ca6c9687a622d73368e016
Harsh K <hkmkeswani@myseneca.ca>
2021-11-25 12:55:33 +0530

    working on day 4

--- advent2020/day-4.ts
+++ advent2020/day-4.ts
@@ -9,19 +9,51 @@ async function processLineByLine() {
     crlfDelay: Infinity,
   });
 
-  let data: string[][] = [];
-  let idx: number = 0;
+  let passports: string[][] = [];
+  let data: string[] = [];
   for await (const line of rl) {
     if (line !== "") {
-      data[idx] = line.split(" ");
+      let fields: string[] = line.split(" ");
+      for (let field of fields) {
+        data.push(field);
+      }
     } else {
-      idx++;
+      passports.push(data);
+      data = [];
     }
   }
+  passports.push(data);
 
-  for await (const item of data) {
-    console.log(item);
+  const requirements: string[] = [
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+  ];
+
+  let valid: number = 0;
+  for (const passport of passports) {
+    let isValid: boolean = false;
+    for (const req of requirements) {
+      for (const field of passport) {
+        isValid = false;
+        if (field.split(":")[0] === req) {
+          isValid = true;
+          break;
+        }
+      }
+      if (!isValid) {
+        break;
+      }
+    }
+    if (isValid) {
+      valid++;
+    }
   }
+  console.log(valid);
 }
 
 async function processLineByLine2() {
@@ -32,11 +64,144 @@ async function processLineByLine2() {
     crlfDelay: Infinity,
   });
 
+  let passports: string[][] = [];
   let data: string[] = [];
   for await (const line of rl) {
-    data.push(line);
+    if (line !== "") {
+      let fields: string[] = line.split(" ");
+      for (let field of fields) {
+        data.push(field);
+      }
+    } else {
+      passports.push(data);
+      data = [];
+      break;
+    }
   }
+  passports.push(data);
+
+  const requirements: string[] = [
+    "iyr",
+    "byr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+  ];
+
+  let valid: number = 0;
+  for (const passport of passports) {
+    let isValid: boolean = false;
+    // console.log(passport)
+    for (const req of requirements) {
+      for (const field of passport) {
+        isValid = false;
+        let item: string[] = field.split(":");
+        console.log(item);
+        if (item[0] === req) {
+          console.log("ok");
+          switch (item[0]) {
+            case "byr":
+              if (
+                item[1].length == 4 &&
+                Number(item[1]) >= 1920 &&
+                Number(item[1]) <= 2002
+              ) {
+                isValid = true;
+              } else {
+                isValid = false;
+              }
+              break;
+            case "iyr":
+              if (
+                item[1].length == 4 &&
+                Number(item[1]) >= 2010 &&
+                Number(item[1]) <= 2020
+              ) {
+                isValid = true;
+              } else {
+                isValid = false;
+              }
+              break;
+            case "eyr":
+              if (
+                item[1].length == 4 &&
+                Number(item[1]) >= 2020 &&
+                Number(item[1]) <= 2030
+              ) {
+                isValid = true;
+              } else {
+                isValid = false;
+              }
+              break;
+            case "hgt":
+              if (!isNaN(Number(item[1]))) {
+                if (
+                  (item[1].includes("cm") &&
+                    Number(item[1]) >= 150 &&
+                    Number(item[1]) <= 193) ||
+                  (item[1].includes("in") &&
+                    Number(item[1]) >= 59 &&
+                    Number(item[1]) <= 76)
+                ) {
+                  isValid = true;
+                } else {
+                  isValid = false;
+                }
+              } else {
+                isValid = false;
+              }
+              break;
+            case "hcl":
+              if (item[1].length == 7 && /^[a-f0-9]+$/.test(item[1])) {
+                isValid = true;
+              } else {
+                isValid = false;
+              }
+              break;
+            case "ecl":
+              if (
+                item[1] == "amb" ||
+                item[1] == "blu" ||
+                item[1] == "brn" ||
+                item[1] == "gry" ||
+                item[1] == "grn" ||
+                item[1] == "hzl" ||
+                item[1] == "oth"
+              ) {
+                isValid = true;
+              } else {
+                isValid = false;
+              }
+              break;
+            case "pid":
+              if (!isNaN(Number(item[1])) && item[1].length == 9) {
+                isValid = true;
+              } else {
+                isValid = false;
+              }
+              break;
+            default:
+              isValid = false;
+              break;
+          }
+          break;
+        }
+        if (!isValid) {
+          break;
+        }
+      }
+      if (!isValid) {
+        break;
+      }
+    }
+    if (isValid) {
+      valid++;
+    }
+  }
+  console.log(valid);
 }
 
-processLineByLine();
-// processLineByLine2();
+// processLineByLine();
+processLineByLine2();
