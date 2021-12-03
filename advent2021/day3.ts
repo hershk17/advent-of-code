8a323512ed8ffd7f31d11a0d8f9abecd32f2a69f
Harsh K <hkmkeswani@myseneca.ca>
2021-12-03 16:54:29 +0530

    day3: completed

--- advent2021/day3.ts
+++ advent2021/day3.ts
@@ -30,6 +30,33 @@ async function processLineByLine() {
   console.log(parseInt(gamma, 2) * parseInt(epsilon, 2));
 }
 
+function filter_data_by_index(
+  data: string[],
+  idx: number,
+  mostCommon: boolean = true
+) {
+  if (data.length === 1) {
+    return data[0];
+  }
+
+  let cnt: number = 0;
+  for (const line of data) {
+    cnt += Number(line[idx]);
+  }
+
+  let filtered: string[] = [];
+  if (mostCommon) {
+    for (const line of data)
+      if (Number(line[idx]) === Number(cnt >= data.length / 2))
+        filtered.push(line);
+  } else {
+    for (const line of data)
+      if (Number(line[idx]) !== Number(cnt >= data.length / 2))
+        filtered.push(line);
+  }
+  return filter_data_by_index(filtered, idx + 1, mostCommon);
+}
+
 async function processLineByLine2() {
   const fileStream = fs.createReadStream("inputs/day3.txt");
 
@@ -38,25 +65,17 @@ async function processLineByLine2() {
     crlfDelay: Infinity,
   });
 
-  const n: number = 12;
-  let data: number[] = new Array(n).fill(0);
+  let data: string[] = [];
   let cnt: number = 0;
   for await (const line of rl) {
-    for (let i = 0; i < line.length; i++) {
-      data[i] += Number(line[i]);
-    }
+    data.push(line);
     cnt++;
   }
 
-  let gamma: string = "";
-  let epsilon: string = "";
-  data.forEach((num) => {
-    let result: boolean = num > cnt / 2;
-    gamma += Number(result);
-    epsilon += Number(!result);
-  });
-
-  console.log(parseInt(gamma, 2) * parseInt(epsilon, 2));
+  console.log(
+    parseInt(filter_data_by_index(data, 0), 2) *
+      parseInt(filter_data_by_index(data, 0, false), 2)
+  );
 }
 
 // processLineByLine();
