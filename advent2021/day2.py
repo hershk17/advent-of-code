69e6d24463fe0681323a4ee5432f8afec8ce1ade
Harsh K <hkmkeswani@myseneca.ca>
2021-12-04 21:52:48 +0530

    ported day 2 to python

--- advent2021/day2.py
+++ advent2021/day2.py
@@ -1,7 +1,34 @@
-file=open("inputs/day1.txt","rt")
-data=file.readlines().split()
-h=0
-for i in range(len(data)):
-    if int(data[i]) > int(data[i-1]):        
-        cnt+=1
-print(cnt)
+def part1():
+    file=open("inputs/day2.txt","rt")
+    data=file.readlines()
+    h=0
+    d=0
+    for i in range(len(data)):
+        fields=data[i].split()
+        if fields[0] == 'forward':
+            h+=int(fields[1])
+        elif fields[0] == 'up':
+            d-=int(fields[1])
+        elif fields[0] == 'down':
+            d+=int(fields[1])
+    print(h*d)
+
+def part2():
+    file=open("inputs/day2.txt","rt")
+    data=file.readlines()
+    h=0
+    d=0
+    a=0
+    for i in range(len(data)):
+        fields=data[i].split()
+        if fields[0] == 'forward':
+            h+=int(fields[1])
+            d+=a*int(fields[1])
+        elif fields[0] == 'up':
+            a-=int(fields[1])
+        elif fields[0] == 'down':
+            a+=int(fields[1])
+    print(h*d)
+
+part1()
+part2()
