f80781097a69e3d2ea6497f388f8ad0a7fcd2b4f
Harsh K <hkmkeswani@myseneca.ca>
2021-12-06 02:55:40 +0530

    day5: completed

--- advent2021/day5.py
+++ advent2021/day5.py
@@ -10,8 +10,7 @@ for line in lines:
     u=(re.compile(' -> |,')).split(line)
     for i in u:
         data.append(int(i))
-    if data[0] == data[2] or data[1] == data[3]:
-        points.append(data)
+    points.append(data)
 
 pattern = []
 factor = 1000
@@ -43,11 +42,45 @@ def part1():
         for j in i:
             if j >= 2:
                 cnt+=1
-    return cnt
+    print(cnt)
 
 
 def part2():
-    print(part1())
+    for point in points:
+        if point[1] == point[3]:
+            if point[0] <= point[2]:
+                for j in range(point[0],point[2]+1):
+                    pattern[point[1]][j]+=1
+            else:
+                for j in range(point[0],point[2]-1,-1):
+                    pattern[point[1]][j]+=1
+        elif point[0] == point[2]:
+            if point[1] <= point[3]:
+                for j in range(point[1],point[3]+1):
+                    pattern[j][point[0]]+=1
+            else:
+                for j in range(point[1],point[3]-1,-1):
+                    pattern[j][point[0]]+=1
+        else:
+            if point[0] <= point[2] and point[1] <= point[3]:
+                for i,j in zip(range(point[0],point[2]+1), range(point[1],point[3]+1)):
+                    pattern[j][i]+=1
+            elif point[0] <= point[2] and point[1] >= point[3]:
+                for i,j in zip(range(point[0],point[2]+1), range(point[1],point[3]-1,-1)):
+                    pattern[j][i]+=1
+            elif point[0] >= point[2] and point[1] <= point[3]:
+                for i,j in zip(range(point[0],point[2]-1,-1), range(point[1],point[3]+1)):
+                    pattern[j][i]+=1
+            else:
+                for i,j in zip(range(point[0],point[2]-1,-1), range(point[1],point[3]-1,-1)):
+                    pattern[j][i]+=1
+    
+    cnt = 0
+    for i in pattern:
+        for j in i:
+            if j >= 2:
+                cnt+=1
+    print(cnt)
 
-print(part1())
-print(part2())
+# part1()
+part2()
