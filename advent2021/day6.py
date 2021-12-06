f54591c3f753f7d2e77badab4dfb4c2a0620bd8a
Harsh K <hkmkeswani@myseneca.ca>
2021-12-06 13:42:20 +0530

    day6: completed p1

--- advent2021/day6.py
+++ advent2021/day6.py
@@ -1,10 +1,21 @@
 file=open("inputs/day6.txt","rt")
 lines=file.readlines()
 
+lf = list(map(int,lines[0].split(',')))
 
+
+def simulate_day():
+    l = len(lf)
+    for i in range(l):
+        lf[i]-=1
+        if lf[i] < 0:
+            lf[i] = 6
+            lf.append(8)
 
 def part1():
-    
+    for i in range(80):
+        simulate_day()
+    print(len(lf))
     
 def part2():
     pass
